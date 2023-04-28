4792258e7834f8bb927c67a1337dfedccda1003e
Geovany Viteri <gevisa82@hotmail.com>
2023-04-28 16:18:22 -0500

    Fin de sección 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <h3>
+      Counter:{{counter}}
+    </h3>
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="reset()" >Reset</button>
+    <button (click)="increaseBy(-1)" >-1</button>
+
+  `
+})
+
+export class CounterComponent  {
+  public counter = 10;
+  constructor() { }
+
+  increaseBy(value:number):void{
+
+    this.counter += value;
+
+   }
+
+   reset():void{
+    this.counter = 10;
+   }
+
+
+}
---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  public name: string = 'iroman';
+  public age: number = 45;
+
+  get capitalizedName():string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string {
+
+    return `${ this.name }-${ this.age }`;
+  }
+
+  changeHero():void {
+   this.name = 'Spiderman';
+  }
+  changeAge():void {
+    this.age = 30;
+  }
+  resetForm():void {
+    this.name = 'Iroman';
+    this.age = 45;
+
+    // document.querySelectorAll('h1').forEach( element => {
+    //   element.innerHTML = '<h1>Desde Angular</h1>';
+    // });
+  }
+
+}
